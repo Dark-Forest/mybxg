e50a4ccb86ecf463f8468fb86a5c32754a0cf790
DarkForest <hongchenranzhi@126.com>
2017-08-31 13:25:25 +0800

    讲师查看功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/8/31.
  */
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
     //调用后台接口获取列表数据
     $.ajax({
         type: 'get',
@@ -12,6 +12,35 @@ define(['jquery','template'],function($,template){
             //解析数据渲染页面
             var html = template('teacherTpl',{list:data.result});
             $('#teacherInfo').html(html);
+
+            //绑定预览单击事件（绑定这个按钮的时候，是后台返回的数据渲染的页面）
+            $('.preview').click(function(){
+                //alert('123');
+                //获取当前记录ID
+                var td = $(this).closest('td');
+                var tcId = td.attr('data-tcId');
+                //根据ID查询数据
+                $.ajax({
+                    type : 'get',
+                    url : '/api/teacher/view',
+                    data : {tc_id : tcId},
+                    dataType : 'json',
+                    success : function(data){
+                        //console.log(data);
+                        //解析数据渲染页面
+                        var html = template('modalTpl',data.result);
+                        $("#modalInfo").html(html);
+                        //显示弹窗
+                        $('#teacherModal').modal();
+                    }
+                });
+            });
         }
     });
 });
+
+
+
+
+
+
