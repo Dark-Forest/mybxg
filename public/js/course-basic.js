b30210b8e9940d6302379272f35c61251cf2e35e
DarkForest <hongchenranzhi@126.com>
2017-09-04 19:59:44 +0800

    添加和编辑课程页面共享

---
+++ public/js/course-basic.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2017/9/4.
+ */
+define(['jquery','template','util'],function($,template,util){
+    // 设置导航菜单选中
+    util.setMenu('/course/add');
+    //获取课程ID
+    var csId = util.qs('cs_id');
+    // 获取添加和编辑的标识位
+    var flag = util.qs('flag');
+    //根据Id调用接口，查询课程详细信息
+    $.ajax({
+        type: 'get',
+        url :'/api/course/basic',
+        data :{cs_id : csId},
+        dataType : 'json',
+        success : function(data){
+            // 解析数据，渲染页面
+            if(flag){
+                data.result.operate = '课程编辑';
+            }else{
+                data.result.operate = '课程添加';
+            }
+            var html= template('basicTpl',data.result);
+            $('#basicInfo').html(html);
+        }
+    });
+});
