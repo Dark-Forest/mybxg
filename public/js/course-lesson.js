3e55bd1ec6250ce7e53b45995a3a181c00de3e5a
DarkForest <hongchenranzhi@126.com>
2017-09-06 13:06:20 +0800

    编辑和添加课时共用模态框

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -16,6 +16,33 @@ define(['jquery','template','util','bootstrap','form'],function($,template,util)
             // 渲染数据
             var html = template('lessonTpl',data.result);
             $('#lessonInfo').html(html);
+
+            //处理添加功能 -- 内容为空
+            $('#addBtn').click(function(){
+                //渲染模态框模板
+                var html = template('modalTpl',{operate : '添加课时'});
+                $('#modalInfo').html(html);
+                // 显示弹窗
+                $('#chapterModal').modal();
+            });
+            //处理编辑功能--编辑是列表，不用id
+            $('.editLesson').click(function(){
+                // 先查询数据
+                $.ajax({
+                   type : 'get',
+                    url : '/api/course/chapter/edit',
+                    data :{ct_id : $(this).attr('data-ctId')},
+                    dataType :'json',
+                    success : function(data){
+                        //渲染模态框模板
+                        data.result.operate = '编辑课时';
+                        var html = template('modalTpl',data.result);
+                        $('#modalInfo').html(html);
+                        // 显示弹窗
+                        $('#chapterModal').modal();
+                    }
+                })
+            });
         }
     });
 });
